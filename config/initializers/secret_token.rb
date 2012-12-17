ebdecd60bc11362616d05df92616da7397261105
Masatoshi Itagaki <masatoshi@rails.to>
2012-12-17 22:57:52 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DIYblog::Application.config.secret_key_base = 'fee5abf2f17bc74ca4a244e630c70c8c99dd6393141d5f821998fd9d5a3ba3aa701840ee2111d3890cde82abb6f49ab76e56c85d565ab89d0196ba690cfbd6f8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DIYblog::Application.config.session_store :encrypted_cookie_store, key: '_DIYblog_session'
